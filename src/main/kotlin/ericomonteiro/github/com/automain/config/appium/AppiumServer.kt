bdf96d4d4cd82520e7c690b4f88f28d2c29e4e14
Érico Monteiro <erico.cintra@gmail.com>
2023-03-12 21:03:45 -0300

    refactor appium service

--- src/main/kotlin/ericomonteiro/github/com/automain/config/appium/AppiumServer.kt
+++ src/main/kotlin/ericomonteiro/github/com/automain/config/appium/AppiumServer.kt
@@ -1,10 +1,11 @@
 package ericomonteiro.github.com.automain.config.appium
 
+import io.appium.java_client.service.local.AppiumDriverLocalService
 import io.appium.java_client.service.local.AppiumServiceBuilder
 import java.io.File
 
 object AppiumServer {
-    val service = AppiumServiceBuilder()
+    val service: AppiumDriverLocalService = AppiumServiceBuilder()
         .withAppiumJS(File(AppiumProperties.SERVER_LOCAL))
         .withIPAddress(AppiumProperties.URL.host)
             .usingPort(AppiumProperties.URL.port)
